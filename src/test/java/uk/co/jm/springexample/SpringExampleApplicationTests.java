3cc9c92ce18f3c532088a0a3e0440ac63f6c1402
marcinszpigiel <marcins@motabilityoperations.co.uk>
2021-03-27 16:20:28 +0000

    init

---
+++ src/test/java/uk/co/jm/springexample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.jm.springexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
